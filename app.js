f3eb97af8fed25ffa69f703522808817dc15425f
David Laribee <laribee@gmail.com>
2011-09-22 17:07:47 -0400

    use port given by heroku

--- app.js
+++ app.js
@@ -49,5 +49,7 @@ io.sockets.on('connection', function (socket) {
   });
 });
 
-app.listen(3000);
+var port = process.env.PORT || 3000;
+
+app.listen(port);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
