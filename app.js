6987f0be501986425daa5d9f0defb3ccf12066e5
Dan Lash <danlash@gmail.com>
2011-09-27 22:06:29 -0400

    shut up log!

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.configure('production', function(){
 io.configure(function () {
   io.set("transports", ["xhr-polling"]);
   io.set("polling duration", 10);
+  io.set('log level', 1)
 });
 
 // Routes
