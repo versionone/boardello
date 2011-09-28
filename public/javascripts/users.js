e763f7157b985c1e9b197f5b9f51d6164f8bef69
Dan Lash <danlash@gmail.com>
2011-09-27 22:12:54 -0400

    user model networks itself, remote option prevents network loops, user tracks position as model attributes

--- public/javascripts/users.js
+++ public/javascripts/users.js
@@ -1,8 +1,27 @@
 var User = Backbone.Model.extend({
   defaults: {
-    name: ''
+    name: '',
+    left: 0,
+    top: 0
+  },
+
+  initialize: function(){
+    var model = this;
+    if (!model.id) model.set({id: 1 + Math.random() * 100000000000000000})
+
+    Networking.bind('remote:cursor-movement', function(data) { 
+      if (data.id == model.id)
+        model.set({left: data.left, top: data.top}, { remote: true });
+    });
+
+    _.each(['change:top', 'change:left'], function(topic){
+      model.bind(topic, function(_model, value, options){
+        if (!options.remote)
+          Networking.trigger('cursor-movement', model.toJSON());
+      });
+    });
   }
-})
+});
 
 var Users = Backbone.Collection.extend({
   model: User
@@ -14,15 +33,29 @@ var UserView = Backbone.View.extend({
 	className: 'user',
 
   initialize: function(){
-    _.bindAll(this, 'render')
+    _.bindAll(this, 'render', 'isMe')
+
+    var model = this.model,
+        view = this;
+    model.bind('change', this.render);
+
+    $(document).mousemove(function(e) {
+      if (view.isMe()) model.set({left: e.pageX, top: e.pageY});
+    });
 
   },
 
   render: function() {
-    $el = $(this.el);
+    var $el = $(this.el);
     $el.html(this.model.get('name'));
-    $el.attr('data-username', this.model.get('name'));
+    $el.attr('data-id', this.model.id);
+    $el.css({top: this.model.get('top'), left: this.model.get('left')});
+    $el.show();
     return this;
+  },
+
+  isMe: function(){
+    return this.model.id == $('body').data().userId;
   }
 
 });
